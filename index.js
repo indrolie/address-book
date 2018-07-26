e511174db0d87ebd5c449ade70b37796eabfb056
Indro Lie <Indrolie@gmail.com>
2018-07-26 15:50:23 +0700

    modify localstorage code quality

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
 "use strict";
 
-console.log("JAVASCRIPT");
-
 const buttonAdd = document.getElementById("button-add");
 
-const checkName = () => {
-  const textboxName = document.getElementById("name").value;
+const check = () => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
 
-  if (textboxName == "") {
+  let email = document.getElementById("email").value;
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let checkEmail = pattern.test(email);
+
+  if (checkEmail === false) {
+    alert("Please enter a correct email address!");
+  } else if (name == "") {
     const errorMessage = "Please enter the name";
     document.getElementById("error-name").innerHTML = errorMessage;
-  }
-};
-
-const checkEmail = () => {
-  try {
-    event.preventDefault();
-    let email = document.getElementById("email").value;
-
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let check = pattern.test(email);
-
-    if (check === false) {
-      alert("Please enter a correct email address!");
-    }
-  } catch (error) {
-    document.write(
-      "Sorry for the inconvenience, but we're having a problem. Please try again later."
-    );
+  } else {
+    addContact();
+    getContact();
   }
 };
 
 let contact = [];
 
 const addContact = () => {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let phoneNumber = document.getElementById("phone-number").value;
-  let newContact = [name, email, phoneNumber];
+  let newContact = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phoneNumber: document.getElementById("phone-number").value
+  };
 
   contact.push(newContact);
   addStorage(contact);
@@ -50,25 +41,17 @@ const addStorage = newData => {
 const getContact = () => {
   let getData = localStorage.getItem("Contact_List");
   let contact = JSON.parse(getData);
+
   const storageLength = contact.length;
 
   let i = 0;
   for (i; i < storageLength; i++) {
-    const contactInfo = contact[i];
-
-    document.getElementById("show-name").innerHTML = "Name: " + contactInfo[0];
+    document.getElementById("show-name").innerHTML = "Name: " + contact[i].name;
     document.getElementById("show-email").innerHTML =
-      "Email: " + contactInfo[1];
+      "Email: " + contact[i].email;
     document.getElementById("show-phone-number").innerHTML =
-      "Phone Number: " + contactInfo[2];
+      "Phone Number: " + contact[i].phoneNumber;
   }
 };
 
-const runAll = () => {
-  checkName();
-  checkEmail();
-  addContact();
-  getContact();
-};
-
-buttonAdd.addEventListener("click", runAll);
+buttonAdd.addEventListener("click", check);
