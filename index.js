80e97293e5fbbd44f488511b3038a9e1d2b8e305
Indro Lie <Indrolie@gmail.com>
2018-07-26 22:32:38 +0700

    delete unnecessary code and improve local storage display result

--- index.js
+++ index.js
@@ -1,61 +1,54 @@
 "use strict";
 
-let contact = [];
+let contactList = [];
 
 const buttonAdd = document.getElementById("button-add");
 const list = document.getElementById("list");
 
 const check = () => {
-	const name = document.getElementById("name").value;
-	const email = document.getElementById("email").value;
-	const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	const checkEmail = pattern.test(email);
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const checkEmail = pattern.test(email);
 
-	if (checkEmail === false) {
-		alert("Please enter a correct email address!");
-	} else if (name == "") {
-		const errorMessage = "Please enter the name";
-		document.getElementById("error-name").innerHTML = errorMessage;
-	} else {
-		addContact();
-		getContact();
-	}
+  if (checkEmail === false) {
+    alert("Please enter a correct email address!");
+  } else if (name == "") {
+    const errorMessage = "Please enter the name";
+    document.getElementById("error-name").innerHTML = errorMessage;
+  } else {
+    addContact();
+    getContact();
+  }
 };
 
 const addContact = () => {
-	let newContact = {
-		name: document.getElementById("name").value,
-		email: document.getElementById("email").value,
-		phoneNumber: document.getElementById("phone-number").value
-	};
+  let newContact = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phoneNumber: document.getElementById("phone-number").value
+  };
 
-	contact.push(newContact);
-	addStorage(contact);
-};
-
-const addStorage = newData => {
-	localStorage.setItem("Contact_List", JSON.stringify(newData));
+  contactList.push(newContact);
+  localStorage.setItem("Contact_List", JSON.stringify(contactList));
 };
 
 const getContact = () => {
-	const getData = localStorage.getItem("Contact_List");
-	const contacts = JSON.parse(getData);
+  const getData = localStorage.getItem("Contact_List");
+  const contacts = JSON.parse(getData);
+  list.innerHTML = "";
 
-	const storageLength = contact.length;
+  contacts.map((contact, index) => {
+    const div = document.createElement("div");
 
-	contacts.map((contact, index) => {
-		const div = document.createElement("div");
-
-		div.innerHTML = `
+    div.innerHTML = `
       <div data-id="contact-${index}" class="contact">
         <p>${contact.name}, ${contact.email}, ${contact.phoneNumber}</p>
       </div>
     `.trim();
 
-		list.append(div.firstChild);
-	});
+    list.append(div.firstChild);
+  });
 };
 
 buttonAdd.addEventListener("click", check);
-
-getContact();
