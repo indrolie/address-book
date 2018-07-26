3915541c8d2a3669e34411029230f0f173bac83e
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-26 16:15:02 +0700

    Fix display issue when mapping through the contacts

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
 "use strict";
 
-const buttonAdd = document.getElementById("button-add");
-
-const check = () => {
-  event.preventDefault();
-  const name = document.getElementById("name").value;
-
-  let email = document.getElementById("email").value;
-  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  let checkEmail = pattern.test(email);
-
-  if (checkEmail === false) {
-    alert("Please enter a correct email address!");
-  } else if (name == "") {
-    const errorMessage = "Please enter the name";
-    document.getElementById("error-name").innerHTML = errorMessage;
-  } else {
-    addContact();
-    getContact();
-  }
-};
-
 let contact = [];
 
-const addContact = () => {
-  let newContact = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phoneNumber: document.getElementById("phone-number").value
-  };
+const buttonAdd = document.getElementById("button-add");
+const list = document.getElementById("list");
 
-  contact.push(newContact);
-  addStorage(contact);
+const check = () => {
+	const name = document.getElementById("name").value;
+	const email = document.getElementById("email").value;
+	const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const checkEmail = pattern.test(email);
+
+	if (checkEmail === false) {
+		alert("Please enter a correct email address!");
+	} else if (name == "") {
+		const errorMessage = "Please enter the name";
+		document.getElementById("error-name").innerHTML = errorMessage;
+	} else {
+		addContact();
+		getContact();
+	}
+};
+
+const addContact = () => {
+	let newContact = {
+		name: document.getElementById("name").value,
+		email: document.getElementById("email").value,
+		phoneNumber: document.getElementById("phone-number").value
+	};
+
+	contact.push(newContact);
+	addStorage(contact);
 };
 
 const addStorage = newData => {
-  localStorage.setItem("Contact_List", JSON.stringify(newData));
+	localStorage.setItem("Contact_List", JSON.stringify(newData));
 };
 
 const getContact = () => {
-  let getData = localStorage.getItem("Contact_List");
-  let contact = JSON.parse(getData);
+	const getData = localStorage.getItem("Contact_List");
+	const contacts = JSON.parse(getData);
 
-  const storageLength = contact.length;
+	const storageLength = contact.length;
 
-  let i = 0;
-  for (i; i < storageLength; i++) {
-    document.getElementById("show-name").innerHTML = "Name: " + contact[i].name;
-    document.getElementById("show-email").innerHTML =
-      "Email: " + contact[i].email;
-    document.getElementById("show-phone-number").innerHTML =
-      "Phone Number: " + contact[i].phoneNumber;
-  }
+	contacts.map((contact, index) => {
+		const div = document.createElement("div");
+
+		div.innerHTML = `
+      <div data-id="contact-${index}" class="contact">
+        <p>${contact.name}, ${contact.email}, ${contact.phoneNumber}</p>
+      </div>
+    `.trim();
+
+		list.append(div.firstChild);
+	});
 };
 
 buttonAdd.addEventListener("click", check);
+
+getContact();
