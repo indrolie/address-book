fdc4e9911ee748d93a27f0b8520d6788f6f549b2
Indro Lie <Indrolie@gmail.com>
2018-07-28 14:28:01 +0700

    re-size background image and button,  increase the use of function to decrease code complexity

--- index.js
+++ index.js
@@ -6,20 +6,55 @@ const buttonAdd = document.getElementById("button-add");
 const list = document.getElementById("list");
 
 const check = () => {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const checkEmail = pattern.test(email);
+  checkName();
+  checkEmail();
 
-  if (checkEmail === false) {
-    alert("Please enter a correct email address!");
-  } else if (name == "") {
-    const errorMessage = "Please enter the name";
-    document.getElementById("error-name").innerHTML = errorMessage;
-  } else {
+  if (checkName() !== "" && checkEmail() === true) {
     addContact();
     getContact();
   }
+};
+
+const checkName = () => {
+  const name = document.getElementById("name").value;
+
+  if (name == "") {
+    const errorMessage = "Please enter the name";
+    document.getElementById("error-name").innerHTML = errorMessage;
+  }
+
+  return name;
+};
+
+const checkEmail = () => {
+  const email = document.getElementById("email").value;
+  const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const check = pattern.test(email);
+
+  if (check === false) {
+    const errorMessage = "Please enter a valid email address";
+    document.getElementById("error-email").innerHTML = errorMessage;
+  }
+
+  return check;
+};
+
+const clearAll = () => {
+  const nameErrorMessage = "";
+  const emailErrorMessage = "";
+  const textboxName = "";
+  const textboxEmail = "";
+  const textboxPhoneNumber = "";
+
+  document.getElementById("error-name").innerHTML = nameErrorMessage;
+  document.getElementById("error-email").innerHTML = emailErrorMessage;
+  document.getElementById("name").value = textboxName;
+  document.getElementById("email").value = textboxEmail;
+  document.getElementById("phone-number").value = textboxPhoneNumber;
+};
+
+const addLocalStorage = (name, array) => {
+  localStorage.setItem(name, JSON.stringify(array));
 };
 
 const addContact = () => {
@@ -30,12 +65,18 @@ const addContact = () => {
   };
 
   contactList.push(newContact);
-  localStorage.setItem("Contact_List", JSON.stringify(contactList));
+  addLocalStorage("contact-list", contactList);
+  clearAll();
+};
+
+const getLocalStorage = name => {
+  const getData = localStorage.getItem(name);
+  const contacts = JSON.parse(getData);
+  return contacts;
 };
 
 const getContact = () => {
-  const getData = localStorage.getItem("Contact_List");
-  const contacts = JSON.parse(getData);
+  const contacts = getLocalStorage("contact-list");
   list.innerHTML = "";
 
   contacts.map((contact, index) => {
